5e65884dec2f769f71f43ce7cfaf9f0fc01a8370
Rupali Pasa <148496525+RP202@users.noreply.github.com>
2024-04-30 12:28:20 +0530

    Update calender.js

--- calender.js
+++ calender.js
@@ -9,8 +9,8 @@ const today=new Date(); //shpowing corrent date tim
 console.log(today);
 const weekDays=["Sunday","Monday","Tuesday","Wednesday",
 "Thursday","Friday","Saturday"];
-const months=["January","Febuary","Wednesday","Thrusday"
-,"Friday","Saturday"];
+const months=["January","Febuary","March","April","May","June","july","august","september","october","november","december"];
+
 
 date.innerHTML= today.getDate(); //sare today wale predefined hai
 days.innerHTML= weekDays[today.getDay()];
